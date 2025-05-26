a3705637cebe88ad34f81fa9bf55283f3726ed70
Freezler <rldevries79@hotmail.com>
2025-05-26 21:39:10 +0200

    fix: grid gallery on mobilescreens

--- src/components/LargeWelcome.tsx
+++ src/components/LargeWelcome.tsx
@@ -6,7 +6,7 @@ export function LargeWelcome() {
 
       <div className="flex xl:flex-row flex-col justify-center items-center place-items-center gap-0 max-w-[100svw] h-[100svh]">
 
-        <div className="z-0 flex justify-center items-center mb-8">
+        <div className="z-0 place-items-center grid mb-8">
           <Image />
         </div>
 
@@ -14,7 +14,7 @@ export function LargeWelcome() {
           <div className="text-center" id="border-gradient">
             <div className="glasWrapper">
               <h2 className="bg-clip-text bg-gradient-to-br from-[rgb(65,12,226)] to-[rgba(50,247,241,0.99)] mt-0 font-extrabold text-[clamp(1.2rem,6vw,3rem)] text-transparent leading-tight tracking-tight animate-pulse in oklch">
-                Welcome to My World
+                WEBDEVELOPMENT IS AWESOME!
               </h2>
               <p className="mx-auto mb-5 w-[clamp(300px,50%,500px)] font-semibold text-[clamp(0.8rem,2vw,1.2rem)] leading-7 tracking-tight hyphens-auto">
                 Dive into the universe of creativity and innovation. Explore the endless possibilities that await you in this journey of discovery.
